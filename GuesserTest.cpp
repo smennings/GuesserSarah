e5689de0889af1bea01d19792b9a5b0900fdbda3
Sarah Jennings <marie.sarah.jennings@gmail.com>
2026-04-29 11:24:12 -0700

    quiz 3

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,8 @@ TEST(GuesserTest, smoke_test)
   Guesser object("Secret");
   ASSERT_EQ( 1+1, 2 );
 }
+
+TEST(GuesserTest, match) {
+	Guesser secret("true");
+	ASSERT_EQ(0, secret.match("true"))
+}
